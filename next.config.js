962f6a508f38243c6554d64e9a2fdea79dca6310
PRAFUL KUMAR <96467257+prafulk9155@users.noreply.github.com>
2025-02-12 16:32:32 +0530

    updated code

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['images.unsplash.com'], // Allow images from Unsplash
-    },
-  };
-  
-  module.exports = nextConfig;
-  
--- src/pages/DisplaySection.jsx
+++ src/pages/DisplaySection.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import WebViewer from './WebViewer'
 
 export default function DisplaySection() {
